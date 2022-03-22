d1c0a8fac5c22198aeace8a4a49f88084292ef18
hugosenari <hugosenari@gmail.com>
2022-03-22 01:10:36 -0300

    chore: re-index ../nimpkgs/l/laser

--- master/flake.nix
+++ master/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-Laser-master.flake = false;
+  inputs.src-Laser-master.ref   = "refs/heads/master";
   inputs.src-Laser-master.owner = "numforge";
-  inputs.src-Laser-master.ref   = "master";
   inputs.src-Laser-master.repo  = "laser";
   inputs.src-Laser-master.type  = "github";
   
